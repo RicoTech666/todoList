b25fec7ccd2d37554cadfb3f4891608075706b70
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-22 22:35:04 +0800

    [Jiachen] refactor-1,2: put new task into local storage when adding new task

--- toDoList.js
+++ toDoList.js
@@ -1,9 +1,14 @@
+function _$(className) {
+  return document.getElementsByClassName(className);
+}
+
 function addNewTask(event) {
-	var addBtn = document.getElementsByClassName("add-btn")[0];
-	var newTaskInputBox = document.getElementsByClassName("new-task")[0];
+	var addBtn = _$("add-btn")[0];
+	var newTaskInputBox = _$("new-task")[0];
 	var isEmpty = checkIsEmpty(newTaskInputBox);
 	if (13 === event.keyCode || event.target === addBtn) {
 		if (!isEmpty) {
+      putNewTaskIntoStorage();
 			putNewTaskIntoList();
 			clearInputBox();
 		}
@@ -17,16 +22,27 @@ function checkIsEmpty(inputBox) {
 		return false;
 	}
 }
-
+function putNewTaskIntoStorage() {
+  var newTaskInputBox = _$("new-task")[0];
+  localStorage.setItem(newTaskInputBox.value, true);
+}
 function putNewTaskIntoList() {
-	var newTaskInputBox = document.getElementsByClassName("new-task")[0];
-	var listOfTodo = document.getElementsByClassName("list-of-todo")[0];
+	var newTaskInputBox = _$("new-task")[0];
+	var listOfTodo = _$("list-of-todo")[0];
 	var targetListLine = document.createElement("li");
-	targetListLine.innerHTML = `<input type="checkbox" class="task-status-box"> ${newTaskInputBox.value}`;
+	targetListLine.innerHTML = `<input type="checkbox" class="task-status-box"> ${newTaskInputBox.value.trim()}`;
 	listOfTodo.appendChild(targetListLine);
 }
 
 function clearInputBox() {
-	var newTaskInputBox = document.getElementsByClassName("new-task")[0];
+	var newTaskInputBox = _$("new-task")[0];
 	newTaskInputBox.innerHTML = "";
+}
+
+function getAddPermission(event) {
+  if(event.target === _$("active-btn")[0]) {
+   return true;
+  } else if (event.target === _$("complete-btn")[0]) {
+   return false;
+  }
 }
