17dfe7f81c321b0c7219e4c64c521ca6b9a09c53
RicoTech666 <zhoujiachenbe@gmail.com>
2019-12-23 14:34:46 +0800

    [Jiachen] refactor-1: change ordered list into unodered list, adding list number manually

--- toDoList.js
+++ toDoList.js
@@ -1,5 +1,9 @@
-function _$(className) {
-  return document.getElementsByClassName(className);
+function _$(className, parentNode) {
+	if (parentNode) {
+		return parentNode.getElementsByClassName(className);
+	} else {
+		return document.getElementsByClassName(className);
+	}
 }
 
 function addNewTask(event) {
@@ -8,8 +12,9 @@ function addNewTask(event) {
 	var isEmpty = checkIsEmpty(newTaskInputBox);
 	if (13 === event.keyCode || event.target === addBtn) {
 		if (!isEmpty) {
-      putNewTaskIntoStorage();
-			putNewTaskIntoList();
+			putNewTaskIntoStorage();
+			displayNewTask();
+			setListNumber();
 			clearInputBox();
 		}
 	}
@@ -23,15 +28,27 @@ function checkIsEmpty(inputBox) {
 	}
 }
 function putNewTaskIntoStorage() {
-  var newTaskInputBox = _$("new-task")[0];
-  localStorage.setItem(newTaskInputBox.value, true);
+	var newTaskInputBox = _$("new-task")[0];
+	localStorage.setItem(newTaskInputBox.value, "notDone");
 }
-function putNewTaskIntoList() {
+function displayNewTask() {
 	var newTaskInputBox = _$("new-task")[0];
 	var listOfTodo = _$("list-of-todo")[0];
 	var targetListLine = document.createElement("li");
-	targetListLine.innerHTML = `<input type="checkbox" class="task-status-box"> ${newTaskInputBox.value.trim()}`;
+	targetListLine.innerHTML = `<input type="checkbox" class="task-status-box">
+   <span class="task-content">${newTaskInputBox.value.trim()}</span>`;
 	listOfTodo.appendChild(targetListLine);
+}
+function setListNumber() {
+	var todoLines = _$("list-of-todo")[0].children;
+	Array.prototype.forEach.call(todoLines, (eachListLine, index) => {
+		var listNumber = document.createElement("span");
+		listNumber.setAttribute("class", "task-number");
+		listNumber.innerHTML = `${index + 1}.`;
+		if ("task-status-box" === eachListLine.firstChild.className) {
+			eachListLine.prepend(listNumber);
+		}
+	});
 }
 
 function clearInputBox() {
@@ -39,10 +56,15 @@ function clearInputBox() {
 	newTaskInputBox.innerHTML = "";
 }
 
-function getAddPermission(event) {
-  if(event.target === _$("active-btn")[0]) {
-   return true;
-  } else if (event.target === _$("complete-btn")[0]) {
-   return false;
-  }
+_$("active-btn")[0].addEventListener("click", getDisplayNewTaskPermission, false);
+_$("complete-btn")[0].addEventListener("click", getDisplayNewTaskPermission, false);
+
+function getDisplayNewTaskPermission() {
+	if (window.event.target === _$("active-btn")[0]) {
+		return true;
+	} else if (window.event.target === _$("complete-btn")[0]) {
+		return false;
+	}
 }
+
+
